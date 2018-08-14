e37d830a9522e5576e70834eccb6862a5b06d61b
maisi <maisi1415@gmail.com>
2018-08-14 12:37:02 +0200

    init

---
+++ fanoff.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set localRwFolder=.local-rw-17
+
+:: download rw if needed
+if not exist %localRwFolder% (
+    mkdir %localRwFolder%
+    powershell -Command "Invoke-WebRequest -OutFile %localRwFolder%/rw-17.zip http://rweverything.com/downloads/RwPortableX64V1.7.zip"
+    powershell -Command "Expand-Archive %localRwFolder%/*.zip -DestinationPath %localRwFolder%"
+    del %localRwFolder%\rw-17.zip
+
+    if %errorlevel% NEQ 0 exit /b
+)
+"%localRwFolder%/Win64/Portable/rw" /Command="%~dp0fanoff.rw" /Stdout
+
